f6d9288cf357e09ed8aea865b56ae5601adff023
ypc2e55orj <43885603+ypc2e55orj@users.noreply.github.com>
2020-02-06 19:21:11 +0900

    update

--- submit.php
+++ submit.php
@@ -1,36 +1,71 @@
 <?php
 class Submit {
-    function __construct($gotten_post) {
+    function __construct($gottenPost) {
+        $contents = $this->adjustContents(json_decode($gottenPost, true));
+        $validationStates = $this->validation($contents);
 
+        $response = [
+            "validationStates" => $validationStates,
+            "contents" => $contents,
+            "databaseResult" => ""
+        ];
+
+        if ($this->isValidationPassed($validationStates)) {
+            $databaseResult = $this->insertDatabase($contents);
+
+            $response["databaseResult"] = $databaseResult;
+        } else {
+            $response["databaseResult"] = [
+                "successful" => false,
+                "message" => "vaildation error",
+            ];
+        }
+
+        echo json_encode($response);
     }
 
-    function validation($contents) {
-        $validation_item =  [
-            "is_required_contents_complete" => true,
-            "is_mail_the_same_as_remail" => true,
-            "is_available_email_address" => true,
-            "is_available_phone_number" => true,
-        ];
+    private function adjustContents($contents) {
+        $adjustedContents = $contents;
 
-        // 必須項目の確認
-        $required_contents = [
-            "mail",
-            "re_mail",
-            "name",
-            "type",
-            "content"
-        ];
-
-        foreach($required_content as $required_contents) {
-            if ($contents[$required_content] === "") {
-                $validation_item["is_required_contents_complete"] = false;
-            }
+        // 電話番号欄が空の場合,0で埋める
+        if ($adjustedContents['phoneNumber'] === "") {
+            $adjustedContents['phoneNumber'] = "00000000000";
         }
+
+        // 電話番号のハイフン除去
+        if
+        (
+            preg_match
+            (
+                "/^[0-9]{2,4}-[0-9]{2,9}-[0-9]{3,4}$/",
+                $adjustedContents['phoneNumber']
+            )
+        ) {
+            str_replace(array('-', 'ー'), '', $adjustedContents['phoneNumber']);
+        }
+
+
+        // 年齢欄が空の場合,0歳とする
+        if ($adjustedContents['age'] === "") {
+            $adjustedContents['age'] = 0;
+        }
+
+        return $adjustedContents;
+    }
+
+    private function validation($contents) {
+        $validationItems =  [
+            "isMailTheSameAsReMail" => true,
+            "isEmailAddressAvailable" => true,
+            "isPhoneNumberAvailable" => true,
+            "isNotTypeEmpty" => true,
+            "isNotContentEmpty" => true
+        ];
 
         // メールアドレスの確認
         // 入力と再入力が一致しない場合
-        if ($contents["mail"] !== $contents["re_mail"]) {
-            $validation_item["is_mail_the_same_as_remail"] = false;
+        if ($contents["mail"] !== $contents["reMail"]) {
+            $validationItems["isMailTheSameAsReMail"] = false;
         }
 
         // メールアドレスが有効ではない場合
@@ -42,7 +77,7 @@ class Submit {
                 $contents['mail']
             )
         ) {
-            $validation_item["is_available_email_address"] = false;
+            $validationItems["isEmailAddressAvailable"] = false;
         }
 
         // 電話番号の確認
@@ -52,54 +87,94 @@ class Submit {
             !preg_match
             (
                 "/^[0-9]{2,4}-?[0-9]{2,9}-?[0-9]{3,4}$/",
-                $contents['phone_number']
+                $contents['phoneNumber']
             )
         ) {
-            $validation_item["is_available_phone_number"] = false;
+            $validationItems["isPhoneNumberAvailable"] = false;
         }
 
-        return $validation_item;
+        // 問い合わせ内容の確認
+        // 問い合わせ内容が空の場合
+        if ($contents['type'] === "") {
+            $validationItems["isNotTypeEmpty"] = false;
+        }
+
+        // テキストボックス入力の確認
+        // テキストボックス入力が空の場合
+        if ($contents['content'] === "") {
+            $validationItems["isNotContentEmpty"] = false;
+        }
+
+        return $validationItems;
     }
 
-    function insert_database($insert_data) {
+    private function isValidationPassed($validationItems) {
+        foreach($validationItems as $_ => $state) {
+            if ($state === false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /*
+        CREATE TABLE form
+        (
+            id INT(11) NOT NULL AUTO_INCREMENT,
+            mail VARCHAR(255) NOT NULL, re_mail VARCHAR(255) NOT NULL ,
+            name VARCHAR(255) NOT NULL, age INT(11), phone_number VARCHAR(255),
+            type VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL,
+            PRIMARY KEY(id)
+        )
+        DEFAULT CHARSET=utf8;
+    */
+    private function insertDatabase($insertData) {
         $dsn = 'mysql:dbname=form; host=localhost; charset=utf8;';
         $user = 'form';
         $password = 'form';
-        $table_name = 'form';
+        $tableName = 'form';
 
-        $is_success = true;
+        $sql = "INSERT INTO `{$tableName}`";
+        $sql .=  "(mail, re_mail, name, age, phone_number, type, content)";
+        $sql .= "VALUES";
+        $sql .= "(";
+        // mail: VARCHAR(255)
+        $sql .= "'{$insertData['mail']}',";
+        $sql .= "'{$insertData['reMail']}',";
+        // name: VARCHAR(255)
+        $sql .= "'{$insertData['name']}',";
+        // age: INT(11)
+        $sql .= "{$insertData['age']},";
+        // phoneNumber: VARCHAR(255)
+        $sql .= "'{$insertData['phoneNumber']}',";
+        // type: VARCHAR(255)
+        $sql .= "'{$insertData['type']}',";
+        // content: VARCHAR(255)
+        $sql .= "'{$insertData['content']}'";
+        $sql .= ")";
 
         try {
             $database = new PDO($dsn, $user, $password);
             $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $database->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-            $sql = "INSERT INTO `{$table_name}`";
-            $sql .=  "(mail, re_mail, name, age, phone_number, type, content)";
-            $sql .= "VALUES ";
-            $sql .= "(";
-            $sql .= "{$insert_data['mail']}";
-            $sql .= "{$insert_data['re_mail']}";
-            $sql .= "{$insert_data['name']}";
-            $sql .= "{$insert_data['age']}";
-            $sql .= "{$insert_data['phone_number']}";
-            $sql .= "{$insert_data['type']}";
-            $sql .= "{$insert_data['content']}";
-            $sql .= ")";
-
             $statement = $database->prepare($sql);
             $statement->execute();
-        } catch(PDOException $error) {
-            $is_success = false;
-        }
 
-        return $is_success;
+            return [
+                "successful" => true,
+                "errorMessage" => ""
+            ];
+        } catch(PDOException $error) {
+            return [
+                "successful" => false,
+                "errorMessage" => $error->getMessage()
+            ];
+        }
     }
 
 }
 
-$my_POST = json_decode(file_get_contents("php://input"), true);
-$ret = ["post"=>$my_POST, "info"=>$_SERVER];
-echo json_encode($ret, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-
+new Submit(file_get_contents("php://input"));
 ?>
